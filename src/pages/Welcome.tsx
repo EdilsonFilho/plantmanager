d10e6f283cb743f9a4682e9913559cc69e70d101
EdilsonFilho <edilsonfilhosh@gmail.com>
2021-07-21 19:53:08 -0300

    using wrapper in welcome

--- src/pages/Welcome.tsx
+++ src/pages/Welcome.tsx
@@ -5,7 +5,8 @@ import {
   Image , 
   TouchableOpacity, 
   StyleSheet,
-  Dimensions
+  Dimensions,
+  View
 } from "react-native";
 import { Feather } from '@expo/vector-icons';
 
@@ -16,33 +17,35 @@ import fonts from "../styles/fonts";
 export function Welcome(){
   return(
     <SafeAreaView style={styles.container}>
-        <Text  style={styles.title}>
-          Gerencie {'\n'}
-          suas plantas {'\n'}
-          de forma fácil
-        </Text>
+        <View style={styles.wrapper}>
+          <Text  style={styles.title}>
+            Gerencie {'\n'}
+            suas plantas de {'\n'}
+            forma fácil
+          </Text>
 
-        <Image 
-          source={wateringImg} 
-          style={styles.image}
-          resizeMode="contain"
-        />
-        
-        <Text style={styles.subtitle}>
-          Não esqueça mais de regar suas plantas. 
-          Nós cuidamos de lembrar você sempre que precisar.
-        </Text>
-        
-        <TouchableOpacity
-          style={styles.button}
-          activeOpacity={0.7}
-        > 
-          <Feather 
-            name="chevron-right" 
-            style={styles.buttonIcon}
+          <Image 
+            source={wateringImg} 
+            style={styles.image}
+            resizeMode="contain"
           />
-     
-        </TouchableOpacity> 
+          
+          <Text style={styles.subtitle}>
+            Não esqueça mais de regar suas plantas. 
+            Nós cuidamos de lembrar você sempre que precisar.
+          </Text>
+          
+          <TouchableOpacity
+            style={styles.button}
+            activeOpacity={0.7}
+          > 
+            <Feather 
+              name="chevron-right" 
+              style={styles.buttonIcon}
+            />
+      
+          </TouchableOpacity>
+      </View> 
     </SafeAreaView>
 
       
@@ -53,8 +56,6 @@ export function Welcome(){
 const styles = StyleSheet.create({
     container: {
       flex: 1,
-      alignItems:'center',
-      justifyContent:'space-around'
     },
     wrapper: {
       flex: 1,
