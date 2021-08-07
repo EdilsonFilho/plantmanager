529f058cff2fe300f501591956205c876806cac2
EdilsonFilho <edilsonfilhosh@gmail.com>
2021-08-07 17:59:18 -0300

    continuing screen adjustments PlantSelect

--- src/pages/PlantSelect.tsx
+++ src/pages/PlantSelect.tsx
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useState } from 'react';
 import {
     Text,
     View,
@@ -8,11 +9,27 @@ import {
 import { EnviromentButton } from '../components/EnviromentButton';
 
 import { Header} from '../components/Header';
+import api from '../services/api';
 
 import colors from '../styles/colors';
 import fonts from '../styles/fonts';
 
+interface EnviromentProps{
+    key: string;
+    title: string;
+}
+
 export function PlantSelect(){
+
+    const [enviroments, setEnviroments] = useState<EnviromentProps[]>([]);
+
+    useEffect(() =>{
+        async function fetchEnviroment(){
+            const {data} = await api.get('plants_environments');
+            setEnviroments(data);
+        }
+        fetchEnviroment();
+    },[])
      return(
         <View style={styles.container}>
             <View style={styles.header}>
@@ -27,11 +44,10 @@ export function PlantSelect(){
             </View>
             <View>
                 <FlatList 
-                    data={[1,2,3,4,5]}
+                    data={enviroments}
                     renderItem={({ item }) => (
                         <EnviromentButton 
-                            title="Cozinha" 
-                            active
+                            title={item.title}
                         />
                     )}
                     horizontal
